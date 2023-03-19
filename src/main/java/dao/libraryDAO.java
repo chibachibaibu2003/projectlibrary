a0d0ae288845e9ff3634a62aced1137574e76e74
chibaibuki <ibuchan1122@icloud.com>
2023-03-19 21:47:05 +0900

    アカウント登録機能とログイン機能（管理者、利用者）のDAO,Servlet,jspの追加

--- src/main/java/dao/libraryDAO.java
+++ src/main/java/dao/libraryDAO.java
@@ -10,9 +10,20 @@ import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Base64;
 import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.HttpMethod;
@@ -34,6 +45,64 @@ import util.GenerateHashedPw;
 import util.GenerateSalt;
 
 public class libraryDAO{
+	// 送信元のユーザの情報等を定数で設定する。
+	private static final String FROM = "i.chiba.sys22@morijyobi.ac.jp";
+	private static final String NAME = "Book Laboratory";
+	private static final String PW = "ymibavywcouwvlco";
+	private static final String CHARSET = "UTF-8";
+		
+		// 宛先、件名、本文を引数にメールを送信するメソッド
+	public static void sendMail(String to, String subject,String text, URL url) {
+		Properties property = new Properties();
+
+		// 各種プロパティの設定
+		property.put("mail.smtp.auth", "true");
+		property.put("mail.smtp.starttls.enable", "true");
+		property.put("mail.smtp.host", "smtp.gmail.com");
+		property.put("mail.smtp.port", "587");
+		property.put("mail.smtp.debug", "true");
+
+			// ログイン情報の取得
+		Session session = Session.getInstance(property,new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(FROM,PW);
+			}
+		});
+
+		try{
+			// 送信するメール本体のインスタンス
+			MimeMessage message = new MimeMessage(session);
+
+			// 送信元の設定
+			// 第1引数：送信元アドレス
+			// 第2引数：送信者名
+			message.setFrom(new InternetAddress(FROM, NAME));
+
+			// 送信先の設定
+			// 第1引数：TO,CC,BCCの区分
+			// 第2引数：送信先アドレス
+			Address toAddress = new InternetAddress(to);
+			message.setRecipient(Message.RecipientType.TO, toAddress);
+			// message.setRecipient(Message.RecipientType.CC, toAddress);
+			// message.setRecipient(Message.RecipientType.BCC, toAddress);
+
+			// 件名と本文の設定
+			message.setSubject(subject, CHARSET);
+			message.setText(text+url, CHARSET);
+
+			// 送信実行！
+			Transport.send(message);
+
+			System.out.println("メール送信完了！");
+
+		} catch (MessagingException e){
+			e.printStackTrace();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+		
+	}
+	
 	private static Connection getConnection() throws URISyntaxException, SQLException {
 		try {
 			Class.forName("org.postgresql.Driver");
@@ -126,8 +195,8 @@ public class libraryDAO{
         
     }
 	
-	public static int registerAccount(account user) {
-		String sql="insert into account values(default,?,?,?,?,?)";
+	public static int registerUserAccount(account user) {
+		String sql="insert into account values(default,?,?,?,?,0)";
 		int result=0;
 		
 		String salt=GenerateSalt.getSalt(32);
@@ -141,7 +210,6 @@ public class libraryDAO{
 			pstmt.setString(2,user.getMail());
 			pstmt.setString(3,salt);
 			pstmt.setString(4,hashedPw);
-			pstmt.setInt(5,user.getUser_check());
 
 			result = pstmt.executeUpdate();
 		} catch (SQLException e) {
@@ -154,5 +222,84 @@ public class libraryDAO{
 		return result;
 	}
 	
+	public static int registerAdminAccount(account user) {
+		String sql="insert into account values(default,?,?,?,?,1)";
+		String admin="administrator";
+		int result=0;
+		
+		String salt=GenerateSalt.getSalt(32);
+		String hashedPw=GenerateHashedPw.getSafetyPassword(user.getPass(), salt);
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1,admin);
+			pstmt.setString(2,user.getMail());
+			pstmt.setString(3,salt);
+			pstmt.setString(4,hashedPw);
+
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(result + "件更新しました。");
+		}
+		return result;
+	}
 	
+	public static String getSalt(String mail) {
+		String sql = "SELECT salt FROM account WHERE mail = ?";
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, mail);
+
+			try (ResultSet rs = pstmt.executeQuery()){
+				
+				if(rs.next()) {
+					String salt = rs.getString("salt");
+					return salt;
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public static account login(String mail, String hashedPw) {
+		String sql = "SELECT * FROM account WHERE mail = ? AND pass = ?";
+		
+		try (
+				Connection con = getConnection();
+				PreparedStatement pstmt = con.prepareStatement(sql);
+				){
+			pstmt.setString(1, mail);
+			pstmt.setString(2, hashedPw);
+
+			try (ResultSet rs = pstmt.executeQuery()){
+				
+				if(rs.next()) {
+					int userId=rs.getInt("id");
+					String name=rs.getString("name");
+					String salt=rs.getString("salt");
+					int user_check=rs.getInt("user_check");
+					
+					return new account(userId,name,mail,salt,hashedPw,user_check);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
---
+++ src/main/java/servlet/LoginExcuteServlet.java
@@ -0,0 +1,81 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.libraryDAO;
+import dto.account;
+import util.GenerateHashedPw;
+
+/**
+ * Servlet implementation class Login2servlet
+ */
+@WebServlet("/LoginExcuteServlet")
+public class LoginExcuteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginExcuteServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		String path="";
+		String pass=request.getParameter("pass");
+		String email=request.getParameter("email");
+		
+		if(email==null||pass==null) {
+			path= "WEB-INF/view/login.jsp?error=1";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+			dispatcher.forward(request, response);
+			return;
+		}
+		
+		String salt=libraryDAO.getSalt(email);
+		if(salt==null) {
+			path = "WEB-INF/view/login.jsp?error=1";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+			dispatcher.forward(request, response);
+			return;
+		}
+		String hashPw= GenerateHashedPw.getSafetyPassword(pass, salt);
+		account account=libraryDAO.login(email, hashPw);
+		if(account==null){
+			path= "WEB-INF/view/login.jsp?error=1";
+			RequestDispatcher dispatcher = request.getRequestDispatcher(path);
+			dispatcher.forward(request, response);
+			return;
+		}else if(account.getUser_check()==0) {
+			path="WEB-INF/view/mypage.jsp";
+			RequestDispatcher dispatcher=request.getRequestDispatcher(path);
+			dispatcher.forward(request, response);
+			return;
+		}else if(account.getUser_check()==1){
+			path="WEB-INF/view/administratorhome.jsp";
+			RequestDispatcher dispatcher=request.getRequestDispatcher(path);
+			dispatcher.forward(request, response);
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
